d8c6e48b974f28bb8138bd6f0198d155cca3da15
Kanupriya-sps <kanupriya_rajpal@softprodigy.com>
2024-12-10 13:49:47 +0530

    initial setup

---
+++ Movementor-fitness/Movementor_fitnessApp.swift
@@ -0,0 +1,17 @@
+//
+//  Movementor_fitnessApp.swift
+//  Movementor-fitness
+//
+//  Created by user on 10/12/24.
+//
+
+import SwiftUI
+
+@main
+struct Movementor_fitnessApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Movementor-fitnessTests/Movementor_fitnessTests.swift
@@ -0,0 +1,17 @@
+//
+//  Movementor_fitnessTests.swift
+//  Movementor-fitnessTests
+//
+//  Created by user on 10/12/24.
+//
+
+import Testing
+@testable import Movementor_fitness
+
+struct Movementor_fitnessTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
